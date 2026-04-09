34a975480516fe52c4e2c544f72eceab1473472f
mahek <mahekksingh56@gmail.com>
2026-04-09 16:09:49 +0530

    printed the best longest substring

--- fifth.java
+++ fifth.java
@@ -9,14 +9,18 @@ public class fifth{
     int left=0;
     int right=0;
     int maxlength=0;
+    int start=0;
     for(right=0;right<str.length();right++){
         while(hs.contains(arr[right])){
             hs.remove(arr[left]);
             left++;
         }
         hs.add(arr[right]);
+        if(right-left+1>maxlength){
+            start=left;
         maxlength=Math.max(maxlength,right-left+1);
+        }
     }
-    System.out.println(maxlength);
+    System.out.println(str.substring(start,start+maxlength));
 }
 }
