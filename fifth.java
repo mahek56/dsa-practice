d8466d5ee5c8ec649626a0cb6c3f50abdc38d453
mahek <mahekksingh56@gmail.com>
2026-04-09 15:59:19 +0530

    Implemented longest substring length using sliding window

---
+++ fifth.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class fifth{
+    public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    String str=sc.nextLine();
+    char[] arr=str.toCharArray();
+    HashSet<Character> hs=new HashSet<>();
+    int left=0;
+    int right=0;
+    int maxlength=0;
+    for(right=0;right<str.length();right++){
+        while(hs.contains(arr[right])){
+            hs.remove(arr[left]);
+            left++;
+        }
+        hs.add(arr[right]);
+        maxlength=Math.max(maxlength,right-left+1);
+    }
+    System.out.println(maxlength);
+}
+}
